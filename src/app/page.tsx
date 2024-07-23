5c13a214e7edcb4c02a7974626735b0a7bb0530c
thisisabhinay <50246894+thisisabhinay@users.noreply.github.com>
2024-07-23 14:21:47 +0530

    feat: create new component for latest products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Hero from "@/components/Hero";
+import NewProducts from "@/components/NewProducts"
 export default function Home() {
   return (
-    <Hero/>
+    <>
+      <Hero/>
+      <NewProducts/>
+    </>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ export default async function Hero() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Fashion for a top price!
+            Top fashion for a top price!
           </h1>
           <p className="max-w-screen-sm leading-relaxed text-gray-500 lg:text-lg">
             We sell only the most exclusive and high quality products for you.
